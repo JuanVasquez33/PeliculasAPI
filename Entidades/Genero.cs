0ddb0cc7a3b5dba0fe4bcee4652eb76c694408c0
Juan Vasquez <jpvt2003.03@gmail.com>
2025-06-12 18:11:56 -0500

    Primer commit y primeros ajustes del backend de peliculas

---
+++ Entidades/Genero.cs
@@ -0,0 +1,14 @@
+﻿using PeliculasAPI.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasAPI.Entidades
+{
+    public class Genero
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
+        [PrimeraLetraMayuscula]
+        public required string Nombre { get; set; }
+    }
+}
